569b5b950677a2784f8d17e0c5ed5374ba0403dc
qq576777915 <1@h5.gg>
2018-06-29 20:31:58 +0800

    设置模块Finish

--- app/src/main/java/com/hngy/rjxh/artifactforcar/Activity_Setting/setting_Activity.java
+++ app/src/main/java/com/hngy/rjxh/artifactforcar/Activity_Setting/setting_Activity.java
@@ -2,24 +2,39 @@ package com.hngy.rjxh.artifactforcar.Activity_Setting;
 
 import android.annotation.SuppressLint;
 import android.app.ActionBar;
+import android.app.AlertDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Handler;
+import android.os.Message;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.NavUtils;
 import android.support.v4.app.TaskStackBuilder;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.text.Html;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.SeekBar;
@@ -29,12 +44,23 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.hngy.rjxh.artifactforcar.R;
+import com.hngy.rjxh.artifactforcar.bluetooth.adapter.BlueListAdapter;
+import com.hngy.rjxh.artifactforcar.bluetooth.bean.BlueDevice;
+import com.hngy.rjxh.artifactforcar.bluetooth.task.BlueAcceptTask;
+import com.hngy.rjxh.artifactforcar.bluetooth.task.BlueConnectTask;
+import com.hngy.rjxh.artifactforcar.bluetooth.task.BlueReceiveTask;
+import com.hngy.rjxh.artifactforcar.bluetooth.util.BluetoothUtil;
+import com.hngy.rjxh.artifactforcar.bluetooth.widget.InputDialogFragment;
 
+import java.io.IOException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
-public class setting_Activity extends AppCompatActivity {
+public class setting_Activity extends AppCompatActivity implements
+        View.OnClickListener, AdapterView.OnItemClickListener, CompoundButton.OnCheckedChangeListener,
+        BlueConnectTask.BlueConnectListener, InputDialogFragment.InputCallbacks, BlueAcceptTask.BlueAcceptListener {
 
     private Switch switch_1_1;
     private SeekBar seekBar;
@@ -50,6 +76,16 @@ public class setting_Activity extends AppCompatActivity {
     private CheckBox checkBox1;
     private CheckBox checkBox2;
     private Spinner spinner;
+    private LinearLayout rl_4;
+    
+    private static final String TAG = "setting_Activity";
+    private CheckBox ck_bluetooth;
+    private TextView tv_discovery;
+    private ListView lv_bluetooth;
+    private BluetoothAdapter mBluetooth;
+    private ArrayList<BlueDevice> mDeviceList = new ArrayList<BlueDevice>();
+    private TextView textView19;
+    private RelativeLayout rl_5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,6 +103,8 @@ public class setting_Activity extends AppCompatActivity {
         rl_1 = (RelativeLayout) findViewById(R.id.rl_1);
         rl_2 = (RelativeLayout) findViewById(R.id.rl_2);
         rl_3 = (RelativeLayout) findViewById(R.id.rl_3);
+        rl_4 = (LinearLayout) findViewById(R.id.rl_4);
+        rl_5 = (RelativeLayout) findViewById(R.id.rl_5);
         final ListView list_set = (ListView) findViewById(R.id.list_set);
         LinkedList<modul_1> mData = new LinkedList<>();
         mData.add(new modul_1("1","报警阈值设置"));
@@ -87,14 +125,33 @@ public class setting_Activity extends AppCompatActivity {
                     rl_1.setVisibility(View.VISIBLE);
                     rl_2.setVisibility(View.INVISIBLE);
                     rl_3.setVisibility(View.INVISIBLE);
+                    rl_4.setVisibility(View.INVISIBLE);
+                    rl_5.setVisibility(View.INVISIBLE);
                 } else if (position == 1){
                     rl_2.setVisibility(View.VISIBLE);
                     rl_1.setVisibility(View.INVISIBLE);
                     rl_3.setVisibility(View.INVISIBLE);
+                    rl_4.setVisibility(View.INVISIBLE);
+                    rl_5.setVisibility(View.INVISIBLE);
                 } else if (position == 2){
                     rl_3.setVisibility(View.VISIBLE);
                     rl_2.setVisibility(View.INVISIBLE);
                     rl_1.setVisibility(View.INVISIBLE);
+                    rl_4.setVisibility(View.INVISIBLE);
+                    rl_5.setVisibility(View.INVISIBLE);
+                } else if (position == 3){
+                    rl_3.setVisibility(View.INVISIBLE);
+                    rl_2.setVisibility(View.INVISIBLE);
+                    rl_1.setVisibility(View.INVISIBLE);
+                    rl_4.setVisibility(View.VISIBLE);
+                    rl_5.setVisibility(View.INVISIBLE);
+                } else if (position == 4){
+
+                    rl_3.setVisibility(View.INVISIBLE);
+                    rl_2.setVisibility(View.INVISIBLE);
+                    rl_1.setVisibility(View.INVISIBLE);
+                    rl_4.setVisibility(View.INVISIBLE);
+                    rl_5.setVisibility(View.VISIBLE);
                 }
             }
         });
@@ -233,6 +290,29 @@ public class setting_Activity extends AppCompatActivity {
                 }
             }
         });
+        textView19 = (TextView) findViewById(R.id.textView19);
+        textView19.setText(Html.fromHtml("<u>"+"Git"+"</u>"));
+        textView19.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openURL("https://github.com/XLwn/ArtifactForCar");
+            }
+        });
+
+        bluetoothPermissions();
+        ck_bluetooth = (CheckBox) findViewById(R.id.ck_bluetooth);
+        tv_discovery = (TextView) findViewById(R.id.tv_discovery);
+        lv_bluetooth = (ListView) findViewById(R.id.lv_bluetooth);
+        if (BluetoothUtil.getBlueToothStatus(this) == true) {
+            ck_bluetooth.setChecked(true);
+        }
+        ck_bluetooth.setOnCheckedChangeListener(this);
+        tv_discovery.setOnClickListener(this);
+        mBluetooth = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetooth == null) {
+            Toast.makeText(this, "本机未找到蓝牙功能", Toast.LENGTH_SHORT).show();
+            finish();
+        }
     }
 
     @Override
@@ -243,6 +323,276 @@ public class setting_Activity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    // 定义获取基于地理位置的动态权限
+    private void bluetoothPermissions() {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{
+                    android.Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+        }
+    }
+
+    /**
+     * 重写onRequestPermissionsResult方法
+     * 获取动态权限请求的结果,再开启蓝牙
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            if (BluetoothUtil.getBlueToothStatus(this) == true) {
+                ck_bluetooth.setChecked(true);
+            }
+            ck_bluetooth.setOnCheckedChangeListener(this);
+            tv_discovery.setOnClickListener(this);
+            mBluetooth = BluetoothAdapter.getDefaultAdapter();
+            if (mBluetooth == null) {
+                Toast.makeText(this, "本机未找到蓝牙功能", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        } else {
+            Toast.makeText(this, "用户拒绝了权限", Toast.LENGTH_SHORT).show();
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        if (buttonView.getId() == R.id.ck_bluetooth) {
+            if (isChecked == true) {
+                beginDiscovery();
+                Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+                startActivityForResult(intent, 1);
+                // 下面这行代码为服务端需要，客户端不需要
+                mHandler.postDelayed(mAccept, 1000);
+                Toast.makeText(getApplicationContext(),"部分模拟器不具备蓝牙，请尽量用真机测试",Toast.LENGTH_LONG).show();
+            } else {
+                cancelDiscovery();
+                BluetoothUtil.setBlueToothStatus(this, false);
+                mDeviceList.clear();
+                BlueListAdapter adapter = new BlueListAdapter(this, mDeviceList);
+                lv_bluetooth.setAdapter(adapter);
+            }
+        }
+    }
+
+    private Runnable mAccept = new Runnable() {
+        @Override
+        public void run() {
+            if (mBluetooth.getState() == BluetoothAdapter.STATE_ON) {
+                BlueAcceptTask acceptTask = new BlueAcceptTask(true);
+                acceptTask.setBlueAcceptListener(setting_Activity.this);
+                acceptTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+            } else {
+                mHandler.postDelayed(this, 1000);
+            }
+        }
+    };
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.tv_discovery) {
+            beginDiscovery();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        if (requestCode == 1) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this, "允许本地蓝牙被附近的其它蓝牙设备发现", Toast.LENGTH_SHORT).show();
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "不允许蓝牙被附近的其它蓝牙设备发现", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private Runnable mRefresh = new Runnable() {
+        @Override
+        public void run() {
+            beginDiscovery();
+            mHandler.postDelayed(this, 2000);
+        }
+    };
+
+    private void beginDiscovery() {
+        if (mBluetooth.isDiscovering() != true) {
+            mDeviceList.clear();
+            BlueListAdapter adapter = new BlueListAdapter(setting_Activity.this, mDeviceList);
+            lv_bluetooth.setAdapter(adapter);
+            tv_discovery.setText("正在搜索蓝牙设备");
+            mBluetooth.startDiscovery();
+        }
+    }
+
+    private void cancelDiscovery() {
+        mHandler.removeCallbacks(mRefresh);
+        tv_discovery.setText("取消搜索蓝牙设备");
+        if (mBluetooth.isDiscovering() == true) {
+            mBluetooth.cancelDiscovery();
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mHandler.postDelayed(mRefresh, 50);
+        blueReceiver = new BluetoothReceiver();
+        //需要过滤多个动作，则调用IntentFilter对象的addAction添加新动作
+        IntentFilter foundFilter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        foundFilter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        foundFilter.addAction(BluetoothDevice.ACTION_BOND_STATE_CHANGED);
+        registerReceiver(blueReceiver, foundFilter);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        cancelDiscovery();
+        unregisterReceiver(blueReceiver);
+    }
+
+    private BluetoothReceiver blueReceiver;
+
+    private class BluetoothReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            Log.d(TAG, "onReceive action=" + action);
+            // 获得已经搜索到的蓝牙设备
+            if (action.equals(BluetoothDevice.ACTION_FOUND)) {
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                BlueDevice item = new BlueDevice(device.getName(), device.getAddress(), device.getBondState() - 10);
+                mDeviceList.add(item);
+                BlueListAdapter adapter = new BlueListAdapter(setting_Activity.this, mDeviceList);
+                lv_bluetooth.setAdapter(adapter);
+                lv_bluetooth.setOnItemClickListener(setting_Activity.this);
+            } else if (action.equals(BluetoothAdapter.ACTION_DISCOVERY_FINISHED)) {
+                mHandler.removeCallbacks(mRefresh);
+                tv_discovery.setText("蓝牙设备搜索完成");
+            } else if (action.equals(BluetoothDevice.ACTION_BOND_STATE_CHANGED)) {
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                if (device.getBondState() == BluetoothDevice.BOND_BONDING) {
+                    tv_discovery.setText("正在配对" + device.getName());
+                } else if (device.getBondState() == BluetoothDevice.BOND_BONDED) {
+                    tv_discovery.setText("完成配对" + device.getName());
+                    mHandler.postDelayed(mRefresh, 50);
+                } else if (device.getBondState() == BluetoothDevice.BOND_NONE) {
+                    tv_discovery.setText("取消配对" + device.getName());
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        cancelDiscovery();
+        BlueDevice item = mDeviceList.get(position);
+        BluetoothDevice device = mBluetooth.getRemoteDevice(item.address);
+        try {
+            if (device.getBondState() == BluetoothDevice.BOND_NONE) {
+                Method createBondMethod = BluetoothDevice.class.getMethod("createBond");
+                Log.d(TAG, "开始配对");
+                Boolean result = (Boolean) createBondMethod.invoke(device);
+            } else if (device.getBondState() == BluetoothDevice.BOND_BONDED &&
+                    item.state != BlueListAdapter.CONNECTED) {
+                tv_discovery.setText("开始连接");
+                BlueConnectTask connectTask = new BlueConnectTask(item.address);
+                connectTask.setBlueConnectListener(this);
+                connectTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, device);
+            } else if (device.getBondState() == BluetoothDevice.BOND_BONDED &&
+                    item.state == BlueListAdapter.CONNECTED) {
+                tv_discovery.setText("正在发送消息");
+                InputDialogFragment dialog = InputDialogFragment.newInstance(
+                        "", 0, "请输入要发送的消息");
+                String fragTag = getResources().getString(R.string.app_name);
+                dialog.show(getFragmentManager(), fragTag);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            tv_discovery.setText("配对异常：" + e.getMessage());
+        }
+    }
+
+    //向对方发送消息
+    @Override
+    public void onInput(String title, String message, int type) {
+        Log.d(TAG, "onInput message=" + message);
+        Log.d(TAG, "mBlueSocket is " + (mBlueSocket == null ? "null" : "not null"));
+        BluetoothUtil.writeOutputStream(mBlueSocket, message);
+    }
+
+    private BluetoothSocket mBlueSocket;
+
+    //客户端主动连接
+    @Override
+    public void onBlueConnect(String address, BluetoothSocket socket) {
+        mBlueSocket = socket;
+        tv_discovery.setText("连接成功");
+        refreshAddress(address);
+    }
+
+    //刷新已连接的状态
+    private void refreshAddress(String address) {
+        for (int i = 0; i < mDeviceList.size(); i++) {
+            BlueDevice item = mDeviceList.get(i);
+            if (item.address.equals(address) == true) {
+                item.state = BlueListAdapter.CONNECTED;
+                mDeviceList.set(i, item);
+            }
+        }
+        BlueListAdapter adapter = new BlueListAdapter(this, mDeviceList);
+        lv_bluetooth.setAdapter(adapter);
+    }
+
+    //服务端侦听到连接
+    @Override
+    public void onBlueAccept(BluetoothSocket socket) {
+        Log.d(TAG, "onBlueAccept socket is " + (socket == null ? "null" : "not null"));
+        if (socket != null) {
+            mBlueSocket = socket;
+            BluetoothDevice device = mBlueSocket.getRemoteDevice();
+            refreshAddress(device.getAddress());
+            BlueReceiveTask receive = new BlueReceiveTask(mBlueSocket, mHandler);
+            receive.start();
+        }
+    }
+
+    //收到对方发来的消息
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            if (msg.what == 0) {
+                byte[] readBuf = (byte[]) msg.obj;
+                String readMessage = new String(readBuf, 0, msg.arg1);
+                Log.d(TAG, "handleMessage readMessage=" + readMessage);
+                AlertDialog.Builder builder = new AlertDialog.Builder(setting_Activity.this);
+                builder.setTitle("我收到消息啦").setMessage(readMessage).setPositiveButton("确定", null);
+                builder.create().show();
+            }
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mBlueSocket != null) {
+            try {
+                mBlueSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void openURL(String s) {
+        Intent intent = new Intent();
+        intent.setAction("android.intent.action.VIEW");
+        Uri content_url = Uri.parse(s);
+        intent.setData(content_url);
+        startActivity(intent);
     }
 
 }
